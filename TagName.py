f0c4c71181297857c16a2583d039fb4baf61b496
cyberpac38 <cyberpac38@gmail.com>
2025-04-15 15:42:51 -0400

    Testing Branch

---
+++ TagName.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+options = Options()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=options)
+
+driver.get("https://learning.postman.com/")
+
+alllinks = driver.find_elements(By.TAG_NAME, "a")
+print(len(alllinks))
+
+for link in alllinks:
+    url = link.get_attribute("href")
+    print(url)
